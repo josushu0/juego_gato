119ab3f1d0466ddac1c884c5b5aeaa51965be85b
Josué Martell <josue.martell17@gmail.com>
2021-02-15 22:35:20 -0600

    Draw the board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let board = ['x'; 9];
+    println!("-----Juego de Gato-----");
+    draw_board(&board);
 }
+
+fn draw_board(board: &[char; 9]) {
+    for i in 0..2 {
+        print!("{} | ", board[i]);
+    }
+    println!("{} ", board[2]);
+    println!("---------");
+    for i in 3..5 {
+        print!("{} | ", board[i]);
+    }
+    println!("{} ", board[5]);
+    println!("---------");
+    for i in 6..8 {
+        print!("{} | ", board[i]);
+    }
+    println!("{} ", board[8]);
+}
+
