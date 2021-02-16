b505308d54330a597608d65af0344c6fd281e655
Josué Martell <josue.martell17@gmail.com>
2021-02-15 23:08:09 -0600

    Choice for first or second

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
+use std::io;
+
 fn main() {
-    let board = ['x'; 9];
+    let board = [' '; 9];
     println!("-----Juego de Gato-----");
-    draw_board(&board);
+    println!("Jugar primer turno? s/n");
+    loop {
+        let mut answer = String::new();
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+        if answer == "s" {
+            next_turn(board);
+            break;
+        }
+        else if answer == "n" {
+            ia_turn(&board);
+            next_turn(board);
+            break;
+        }
+        else {
+            println!("Escriba una opción valida");
+        }
+    }
+}
+
+fn next_turn(board: [char; 9]) {
+
+}
+
+fn ia_turn(board: &[char; 9]) {
+
 }
 
 fn draw_board(board: &[char; 9]) {
